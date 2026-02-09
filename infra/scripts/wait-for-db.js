0d3aa0218b5929ad46997d23088368aa934dfda6
Tallys Cesar <98368130+TallysCesarGB@users.noreply.github.com>
2026-02-09 21:21:06 +0000

    Creates an ellipsis animation while waiting at the bank

--- infra/scripts/wait-for-db.js
+++ infra/scripts/wait-for-db.js
@@ -1,5 +1,23 @@
 const { exec } = require("node:child_process");
 
+let dotCount = 0;
+let animationInterval;
+
+function animateDots() {
+  process.stdout.write("\n\n ⏳ Waiting on Postgres to accept connections");
+
+  animationInterval = setInterval(() => {
+    process.stdout.write("\r");
+    process.stdout.write("\n\n ⏳ Waiting on Postgres to accept connections");
+
+    dotCount = (dotCount + 1) % 4;
+
+    for (let i = 0; i < dotCount; i++) {
+      process.stdout.write(".");
+    }
+  }, 500);
+}
+
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
