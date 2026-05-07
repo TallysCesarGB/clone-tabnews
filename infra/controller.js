76c9802e0d04cbdd241dcf66c962fbd5bf608f39
Tallys Cesar <98368130+TallysCesarGB@users.noreply.github.com>
2026-05-07 13:06:13 +0000

    feat: use `ServiceError` in `database.js`

---
+++ infra/controller.js
@@ -0,0 +1,26 @@
+import { InternalServerError, MethodNotAllowedError } from "infra/errors.js";
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onErrorHandler(err, request, response) {
+  const publicErrorObject = new InternalServerError({
+    statusCode: err.statusCode,
+    cause: err,
+  });
+
+  console.error(publicErrorObject);
+
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+const controller = {
+  errorHandlers: {
+    onNoMatch: onNoMatchHandler,
+    onError: onErrorHandler,
+  }
+};
+
+export default controller;
--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,49 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Unnexpected internal error", {
       cause,
     });
     this.name = "InternalServerError";
     this.action = "Try again later or contact support if the problem persists.";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Service currently unavailable.", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Check if the dependent services are running and healthy.";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Method not allowed");
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Check the API documentation for the allowed methods and endpoints.";
+    this.statusCode = 405;
   }
 
   toJSON() {
