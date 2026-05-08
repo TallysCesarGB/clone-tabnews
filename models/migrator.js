d294797c27739070f0dfbcb3ae7c3344f30a772e
Tallys Cesar <98368130+TallysCesarGB@users.noreply.github.com>
2026-05-08 14:09:57 +0000

    feat: creat `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,51 @@
+import { runner as migrationRunner } from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database";
+
+const defaultMigrationsOptions = {
+  dryRun: true,
+  dir: join("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dbClient,
+    });
+    await dbClient.end();
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dbClient,
+      dryRun: false,
+    });
+    await dbClient.end();
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
