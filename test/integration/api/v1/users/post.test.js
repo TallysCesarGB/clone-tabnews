1411b944d3ab8ba9bf8d914053f86281637e5c83
Tallys Cesar <98368130+TallysCesarGB@users.noreply.github.com>
2026-05-17 22:46:41 +0000

    feat: add user creation API and integrate UUID validation

--- test/integration/api/v1/users/post.test.js
+++ test/integration/api/v1/users/post.test.js
@@ -1,5 +1,5 @@
+import { version as uuidVersion } from "uuid";
 import orchestrator from "test/orchestrator.js";
-import database from "infra/database.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -10,18 +10,31 @@ beforeAll(async () => {
 describe("POST /api/v1/users", () => {
   describe("Anonymous user", () => {
     test("With unique and valid data", async () => {
-      await database.query({
-        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
-        values: ["Tata", "tata@email.com", "password123"],
-      });
-
-      const users = await database.query("SELECT * FROM users;");
-      console.log(users.rows);
-
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "Tata",
+          email: "tata@email.com",
+          password: "password123",
+        }),
       });
       expect(response.status).toBe(201);
+
+      const { newUser } = await response.json();
+      expect(newUser).toEqual({
+        id: newUser.id,
+        username: "Tata",
+        email: "tata@email.com",
+        password: "password123",
+        created_at: newUser.created_at,
+        updated_at: newUser.updated_at,
+      });
+      expect(uuidVersion(newUser.id)).toBe(4);
+      expect(Date.parse(newUser.created_at)).not.toBeNaN();
+      expect(Date.parse(newUser.updated_at)).not.toBeNaN();
     });
   });
 });
